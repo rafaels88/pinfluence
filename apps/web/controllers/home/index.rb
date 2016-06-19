6da2f68d09265a605911b91d64f6a1304566f83b
Rafael Soares <rafaeltravel88@gmail.com>
2016-06-19 14:13:30 -0300

    Fix timeline bar layout

--- apps/web/controllers/home/index.rb
+++ apps/web/controllers/home/index.rb
@@ -2,18 +2,10 @@ module Web::Controllers::Home
   class Index
     include Web::Action
 
-    expose :influencers
     expose :gmaps_api_key
 
     def call(params)
-      @influencers = repository.all
       @gmaps_api_key = ENV['GOOGLE_MAPS_API_KEY']
-    end
-
-    private
-
-    def repository
-      InfluencerRepository
     end
   end
 end
