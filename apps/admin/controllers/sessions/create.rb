72a7b3bc68a6a0a8eca278bea173ae25368d630f
Rafael Soares <rafaeltravel88@gmail.com>
2017-01-07 23:48:49 -0200

    Change implementation of VerifyAuthCredentials to FindUserByAuthCredentials interactor

--- apps/admin/controllers/sessions/create.rb
+++ apps/admin/controllers/sessions/create.rb
@@ -2,10 +2,11 @@ module Admin::Controllers::Sessions
   class Create
     include Admin::Action
 
-    def call(params)
-      user = UserRepository.new.find_by_email(params[:session][:email])
+    def call(_)
+      user = FindUserByAuthCredentials.call(email: requested_email,
+                                            password: requested_password)
 
-      if user && user.password == params[:session][:password_plain]
+      if !user.nil?
         session[:user_id] = user.id
         redirect_to routes.home_path
       else
@@ -14,6 +15,14 @@ module Admin::Controllers::Sessions
     end
 
     private
+
+    def requested_email
+      params[:session][:email]
+    end
+
+    def requested_password
+      params[:session][:password_plain]
+    end
 
     def authenticate!; end
   end
